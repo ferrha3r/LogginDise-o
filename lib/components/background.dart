213aaa6189cc217f3816709746832c69be654c25
ferrha3r <106357375+ferrha3r@users.noreply.github.com>
2023-05-21 17:48:04 -0600

    Add files via upload

---
+++ lib/components/background.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key? key,
+    required this.child,
+    this.topImage = "assets/images/main_top.png",
+    this.bottomImage = "assets/images/login_bottom.png",
+  }) : super(key: key);
+
+  final String topImage, bottomImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                topImage,
+                width: 120,
+              ),
+            ),
+            // Positioned(
+            //   bottom: 0,
+            //   right: 0,
+            //   child: Image.asset(bottomImage, width: 120),
+            // ),
+            SafeArea(child: child),
+          ],
+        ),
+      ),
+      bottomNavigationBar: GNav(
+        gap: 8,
+        tabs: const [
+          GButton(
+            icon: Icons.account_circle,
+            text: 'Estado',
+            ),
+          GButton(
+            icon: Icons.toys,
+            text: 'Viajes',
+            ),
+          GButton(
+            icon: Icons.schedule,
+            text: 'Historia',
+            ),
+          GButton(
+            icon: Icons.settings,
+            text: 'Configuración',
+            ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 92, 132, 177);
+const kPrimaryLightColor = Color.fromARGB(255, 232, 234, 255);
+
+const double defaultPadding = 16.0;
